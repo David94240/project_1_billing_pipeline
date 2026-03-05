d766435e37b3dbbf0ce5df6c0735f9385236533a
David <david.charoy@gmail.com>
2026-03-05 14:10:28 +0100

    Commit testing des log de suppression et correction des éléments du data frame pandas

--- pipeline_facturation.py
+++ pipeline_facturation.py
@@ -17,22 +17,39 @@ def clean_data(df):
     """
     Nettoie les données de facturation.
     """
+
+    # Log initial count of duplicates in the incoming DataFrame
+    initial_duplicates = df.duplicated(subset=['id_facture'], keep=False).sum()
+    logging.info(f"Début du nettoyage: {initial_duplicates} doublons initiaux détectés sur id_facture avant tout traitement.")
+
     # 1. Compter les montants négatifs/manquants AVANT suppression
     montants_invalides = df[df['montant_HT'].isna() | (df['montant_HT'] < 0)]
     logging.info(f"Suppression de {len(montants_invalides)} lignes avec montants invalides.")
 
     # 2. Dates manquantes
+    # Convertir explicitement les chaînes vides ou avec seulement des espaces en pd.NaT
+    df['date_facture'] = df['date_facture'].replace(r'^\s*$', pd.NaT, regex=True)
+    date_facturation_empty = df[df['date_facture'].isna()]
+    logging.info(f"Nombre de lignes avec date_facture vide : {len(date_facturation_empty)}")
     df['date_facture'] = df['date_facture'].fillna(pd.Timestamp(datetime.now().date()))
-    logging.info(f"Remplacement de {df['date_facture'].isna().sum()} dates manquantes par la date du jour.")
 
     # 3. Suppression des montants HT invalides
+    montant_HT_invalid = df[df['montant_HT'].isna() | (df['montant_HT'] < 0)]
     df = df[(df['montant_HT'].notna()) & (df['montant_HT'] >= 0)]
+    logging.info(f"Suppression de {len(montant_HT_invalid)} lignes avec montants HT invalides.")
+
+    # Suppression des taux_TVA non renseigner et mis à jour des logs
+    taux_tva_missing_count = df['taux_TVA'].isna().sum() # Compter avant la suppression
+    df = df[df['taux_TVA'].notna()]
+    logging.info(f"Suppression de {taux_tva_missing_count} lignes avec taux_TVA non renseigner.")
 
     # 4. Normalisation des codes d'actes
     df['code_acte'] = df['code_acte'].str.replace('-', '').str.upper()
+    logging.info(
+        f"Normalisation des codes d'actes en majuscules et sans tirets. Nombre total d'actes uniques après normalisation : {df['code_acte'].nunique()}.")
 
-    # 5. Suppression des doublons
-    doublons = df.duplicated(subset=['id_facture']).sum()
+    # 5. Suppression des doublons qui pourrait rester normalement est à 0
+    doublons = df.duplicated(subset=['id_facture'], keep='first').sum()
     df = df.drop_duplicates(subset=['id_facture'], keep='first')
     logging.info(f"Suppression de {doublons} doublons sur id_facture.")
 
